93412052c042cd7d96f40d92b619f38e9d6f9dbe
Kyle Jablonski <kyle@eventjoy.com>
2017-01-24 08:58:41 -0800

    RecylcerView demo

--- app/src/main/java/com/kdotj/demo/listviewdemo/DemoAdapter.java
+++ app/src/main/java/com/kdotj/demo/listviewdemo/DemoAdapter.java
@@ -1,73 +1,85 @@
+/*
+    Copyright 2017 Kyle Jablonski
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
 package com.kdotj.demo.listviewdemo;
 
-import android.content.Context;
 import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Locale;
 
 /**
- * Demo Adapter with Custom View
+ * Demo RecyclerView Adapter
  */
 
-public class DemoAdapter extends BaseAdapter {
+class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.ContactHolder> {
 
-    private LayoutInflater mInflater;
-    private Context mContext;
     private List<ContactObject> mContacts;
 
-
-    static class ViewHolder{
-        AppCompatTextView mTvName;
-        AppCompatTextView mTvAddress;
-        AppCompatTextView mTvPhone;
-    }
-
-    public DemoAdapter(Context context){
-        mContext = context;
-        mInflater = LayoutInflater.from(context);
+    DemoAdapter(){
         mContacts = ContactObject.getContactList();
     }
 
     @Override
-    public int getCount() {
+    public int getItemCount() {
         return mContacts.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return mContacts.get(i);
+    public ContactHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ContactHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_row, parent, false));
     }
 
     @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
-
-        ViewHolder viewHolder;
-        if(convertView == null){
-            viewHolder = new ViewHolder();
-            convertView = mInflater.inflate(R.layout.contact_row, viewGroup, false);
-            viewHolder.mTvName = (AppCompatTextView) convertView.findViewById(R.id.name);
-            viewHolder.mTvAddress = (AppCompatTextView) convertView.findViewById(R.id.address);
-            viewHolder.mTvPhone = (AppCompatTextView) convertView.findViewById(R.id.phone);
-            convertView.setTag(viewHolder);
-        }else{
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
+    public void onBindViewHolder(ContactHolder holder, int position) {
 
         final ContactObject contactObject = mContacts.get(position);
+        holder.mTvName.setText(String.format(Locale.getDefault(), "%s %s", contactObject.firstName, contactObject.lastName));
+        holder.mTvAddress.setText(contactObject.address);
+        holder.mTvPhone.setText(contactObject.phone);
 
-        viewHolder.mTvName.setText(String.format(Locale.getDefault(), "%s %s", contactObject.firstName, contactObject.lastName));
-        viewHolder.mTvAddress.setText(contactObject.address);
-        viewHolder.mTvPhone.setText(contactObject.phone);
-        return convertView;
+        /*
+            Listener is added to the root of the Layout
+         */
+        holder.mRootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), String.format(Locale.getDefault(), "%s %s selected!", contactObject.firstName, contactObject.lastName), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    static class ContactHolder extends RecyclerView.ViewHolder{
+
+        View mRootView;
+        AppCompatTextView mTvName;
+        AppCompatTextView mTvAddress;
+        AppCompatTextView mTvPhone;
+
+        ContactHolder(View itemView) {
+            super(itemView);
+
+            mRootView = itemView;
+            mTvName = (AppCompatTextView) itemView.findViewById(R.id.name);
+            mTvAddress = (AppCompatTextView) itemView.findViewById(R.id.address);
+            mTvPhone = (AppCompatTextView) itemView.findViewById(R.id.phone);
+        }
     }
 }
--- app/src/main/java/com/kdotj/demo/listviewdemo/MainActivity.java
+++ app/src/main/java/com/kdotj/demo/listviewdemo/MainActivity.java
@@ -1,18 +1,29 @@
+/*
+    Copyright 2017 Kyle Jablonski
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
 package com.kdotj.demo.listviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.Locale;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 /**
- * Sample Activity showing the usage of a ListView with a custom View.
+ * Sample Activity showing the usage of a RecyclerView.
  * <br/>
- * This demo illustrates the use of a BaseAdapter with the ViewHolder pattern.
+ * This demo illustrates the use of a RecyclerView and RecyclerView.Adapter
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -21,19 +32,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(new DemoAdapter(this));
-
-        /*
-            Apply an AdapterView.OnItemClickListener to ListView to handle clicks in a ListView
-            when no children are clickable
-         */
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ContactObject contactObject = (ContactObject) adapterView.getItemAtPosition(i);
-                Toast.makeText(MainActivity.this, String.format(Locale.getDefault(), "%s %s selected!", contactObject.firstName, contactObject.lastName), Toast.LENGTH_SHORT).show();
-            }
-        });
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(new DemoAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 }
